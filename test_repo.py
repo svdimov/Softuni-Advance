655f1e6e08656b932a6a6df6de8b2d4be6112f18
stefoto85 <you@example.com>
2024-09-03 21:57:31 +0300

    Create README.md Merge remote-tracking branch 'origin/main'

--- test_repo.py
+++ test_repo.py
@@ -1,1 +1,8 @@
-print('soft uni')
+a = int(input())
+my_list = []
+for i in range(a,-1,-1):
+    my_list.append(i)
+
+even_list = [even for even in my_list if even % 2 == 0]
+print(even_list)
+print(my_list)
